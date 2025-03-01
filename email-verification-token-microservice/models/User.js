3e0f89f36b250775641c397110c356d509310a3c
Johana Elizalde <njelizalde@uce.edu.ec>
2025-03-01 12:59:37 -0500

    feat(model): 👤 add user model

---
+++ email-verification-token-microservice/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isVerified: { type: Boolean, default: false },
+    verificationToken: { type: String, default: null },
+    verificationTokenExpiry: { type: Date, default: null },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
